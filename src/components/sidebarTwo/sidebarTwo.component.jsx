3a4e2b5977595bfb1009cb7b74daf4954fb317a3
Ion Leahu <forza11879@gmail.com>
2020-07-29 02:19:35 -0400

    update

--- src/components/sidebarTwo/sidebarTwo.component.jsx
+++ src/components/sidebarTwo/sidebarTwo.component.jsx
@@ -2,28 +2,9 @@ import React, { Component } from 'react';
 import './sidebarTwo.styles.scss';
 
 class Sidebartwo extends Component {
-  // state = {
-  //   isActive: true,
-  // };
-
-  // toggleClassTwo = () => {
-  //   const { isActive } = this.state;
-  //   this.setState({ isActive: !isActive });
-  // };
-
   render() {
     return <div className="sidebarTwo"></div>;
   }
-
-  // getOnOffToggleTwo() {
-  //   return this.state.isActive === true
-  //     ? 'fas fa-long-arrow-alt-left'
-  //     : 'fas fa-long-arrow-alt-right';
-  // }
-
-  // getIsActiveStatusTwo() {
-  //   return this.state.isActive === true ? 'sidebarTwo active' : 'sidebarTwo';
-  // }
 }
 
 export default Sidebartwo;
