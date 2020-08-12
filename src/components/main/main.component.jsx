0b5635090f459a85a2573b1bc849df4ab2cb8a67
Ion Leahu <forza11879@gmail.com>
2020-08-11 20:25:35 -0400

    update for react-select

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -3,23 +3,48 @@ import './main.styles.scss';
 import { createChart } from 'lightweight-charts';
 import { getData, getSymbolData } from './utils.js';
 
-import Select from 'react-select';
 import AsyncSelect from 'react-select/async';
 
 class Main extends Component {
   ref = React.createRef();
+  textInput = React.createRef();
+  // textInput = React.useRef(null);
   state = {
     selectedSymbol: null,
+    isClearable: true,
+    symbol: null,
   };
 
-  loadOptions = async (inputText, callback) => {
-    callback(await getSymbolData(inputText));
+  getInputValue() {
+    return this.ref.select.state.inputValue;
+  }
+
+  toggleClearable = () =>
+    this.setState((state) => ({ isClearable: !state.isClearable }));
+
+  loadOptions = async (inputText) => {
+    const symbolData = await getSymbolData(inputText);
+    console.log('symbolDATA: ', symbolData);
+    return symbolData;
   };
 
-  onChange = (selectedSymbol) => {
-    this.setState({
-      selectedSymbol: selectedSymbol || null,
-    });
+  // _handleChange = (event) => {
+  //   this.setState({ value: event.target.value })
+  // }
+
+  onInputChange = (newValue, reason) => {
+    console.log('reason.action :', reason.action);
+    // const { value } = this.props;
+    // console.log('PROPS :', this.props);
+    // console.log('newValue :', newValue);
+    // console.log('newValue :', e.value);
+
+    // removes all the symbols, spaces
+    const selectedSymbol = newValue.replace(/\W/g, '');
+
+    this.setState({ selectedSymbol: selectedSymbol });
+
+    // return selectedSymbol;
   };
 
   componentDidMount() {
@@ -33,16 +58,19 @@ class Main extends Component {
     });
 
     const candleSeries = chart.addCandlestickSeries();
-    // const data = getData();
-    // candleSeries.setData(data);
+    console.log(
+      'DID MOUNT this.state.selectedSymbol:',
+      this.state.selectedSymbol
+    );
 
-    // getData().then((data) => {
-    //   // console.log(data);
-    //   candleSeries.setData(data);
-    // });
+    getData().then((data) => {
+      console.log(data);
+      candleSeries.setData(data);
+    });
   }
 
   render() {
+    const { isClearable } = this.state;
     console.log('console this.state:', this.state);
     console.log(
       'console this.state.selectedSymbol:',
@@ -53,19 +81,27 @@ class Main extends Component {
         <div className="trading">
           <div className="box one">
             <AsyncSelect
-              // value={this.state.selectedSymbol}
-              onChange={this.onChange}
+              defaultOptions
+              cacheOptions
               loadOptions={this.loadOptions}
+              onInputChange={this.handleInputChange}
+              // onChange={this.handleInputChange}
               autoFocus
               noOptionsMessage={() => 'Search symbol'}
               placeholder="Search Symbol"
+              isClearable={isClearable} // allows us to clear the selected value either using the backspace button or the “x” button on the right side of the field
+              clear // Removing all selected options using the clear button
+              pop-value // Removing options using backspace
+              loadingIndicator
             />
-            {/* <Select
-              value={inputValue}
-              onInputChange={this.handleInputChange}
-              onChange={this.handleChange}
-              options={data}
-            /> */}
+
+            {/* {this.state.selectedValue && (
+              <div style={{ marginTop: 20, lineHeight: '25px' }}>
+                <div>
+                  <b>Selected Value: </b> {this.state.selectedValue}
+                </div>
+              </div>
+            )} */}
           </div>
           <div className="box two" ref={this.ref}></div>
         </div>
